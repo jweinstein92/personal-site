36719834dca4a35e9215b9524896fb77e061329b
Josh Weinstein <jwdrexel92@gmail.com>
2018-03-29 14:30:26 -0400

    add experience details

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,10 +38,10 @@ class IndexPage extends React.Component {
           <div className="menu-bars" data-layout="column" data-layout-align="center end">
             <button onClick={this.toggleMenu}><i className="fa fa-bars" /></button>
           </div>
-          <Scrollspy items={ ['profile', 'experience', 'skills', 'travel'] } currentClassName="is-current" offset="1" className={this.state.menuOpen ? 'open' : 'closed'}>
+          <Scrollspy items={ ['profile', 'experience', 'projects', 'travel'] } currentClassName="is-current" className={this.state.menuOpen ? 'open' : 'closed'}>
             <li><a href="#profile">Profile</a></li>
             <li><a href="#experience">Experience</a></li>
-            <li><a href="#skills">Skills</a></li>
+            <li><a href="#projects">Projects</a></li>
             <li><a href="#travel">Travel</a></li>
           </Scrollspy>
         </div>
@@ -85,18 +85,88 @@ class IndexPage extends React.Component {
               <span className="title">Experience</span>
               <span className="description">I have worked with corporations and led a startup</span>
             </div>
-            <div class="experience-details" data-flex data-layout="column">
-
+            <div className="experience-details" data-flex data-layout="column">
+              <div className="professional" data-layout="column">
+                <span className="title">Professional</span>
+                <div className="job" data-layout-gt-sm="row" data-layout-sm="column">
+                  <div className="general" data-flex="33" data-layout="column">
+                    <span className="company">Booksmart Touring</span>
+                    <span className="date">June 2015 - Present</span>
+                  </div>
+                  <div className="details" data-flex="66" data-layout="column">
+                    <span className="position">Co-founder, Lead Engineer</span>
+                    <div className="description">
+                      I co-founded a company with two friends based around helping manage the hectic life on the road of
+                      touring musicians. Starting from the ground up, I helped determine the main technologies we would
+                      use, including <b>Django Rest Framework</b> for the RESTful API and <b>AngularJS</b> with the web
+                      platform. In the almost three years with Booksmart, I have worked on database and API architecture,
+                      database modeling, performance analysis, feature design, translating requirements specifications
+                      into technical specifications, and managing a team of part time developers.
+                    </div>
+                    <a href="https://booksmart.it" target="_blank" title="Booksmart Touring" className="link"><i className="fa fa-link" />https://booksmart.it</a>
+                  </div>
+                </div>
+                <div className="job" data-layout-gt-sm="row" data-layout-sm="column">
+                  <div className="general" data-flex="33" data-layout="column">
+                    <span className="company">Oracle Primavera</span>
+                    <span className="date">April to August, 2014, April to September 2015</span>
+                  </div>
+                  <div className="details" data-flex="66" data-layout="column">
+                    <span className="position">Applications Engineer</span>
+                    <div className="description">
+                      While working for Oracle Primavera, a subsidiary of Oracle, I worked
+                      with various technologies ranging from JavaScript, Git, JQuery, HTML, CSS, Handlebars, and an
+                      internal Javascript component library. I was tasked with developing new UI features to enhance
+                      user experience on their project managment suite.
+                    </div>
+                  </div>
+                </div>
+                <div className="job" data-layout-gt-sm="row" data-layout-sm="column">
+                  <div className="general" data-flex="33" data-layout="column">
+                    <span className="company">Institute for Advanced Study</span>
+                    <span className="date">April 2013 to April 2014</span>
+                  </div>
+                  <div className="details" data-flex="66" data-layout="column">
+                    <span className="position">Junior Web Developer</span>
+                    <div className="description">
+                      Utilizing the content management system <b>Drupal</b>, I helped build multiple web applications
+                      during this position. Specifically, I helped build a new video streaming website which included a
+                      quick page creation tool, automatic poster image creation using ffmpeg, and embedded videos using
+                      &lt;iframe&gt;. I also upgraded their time tracking web application to be more user friendly.
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="education" data-layout="column">
+                <span className="title">Education</span>
+                <div className="school" data-layout-gt-sm="row" data-layout-sm="column">
+                  <div className="general" data-flex="33" data-layout="column">
+                    <span className="company">Drexel University</span>
+                    <span className="date">September 2011 to June 2016</span>
+                  </div>
+                  <div className="details" data-flex="66" data-layout="column">
+                    <span className="position">Bachelor of Science in Computer Science</span>
+                    <div className="description">
+                      I graduated from Drexel University in 2016 with a degree in computer science with a concentration
+                      in <b>Human Computer Interaction</b> and <b>Artificial Intelligence</b>. While there, I took many
+                      classes, some interesting, some not, explored Philadelphia, made a great group of friends, and was
+                      president of the school's chapter of <b>Engineers Without Borders</b>. Our project was to build
+                      water filtration devices in the small village of Miramar in El Salvador. As president, I was in
+                      charge of meetings, managing the various teams we had, and talking with the university and advisors.
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div id="skills" className="section-container" data-layout="column" data-layout-align="start center">
-          <div className="section skills" data-flex data-layout="column">
+        <div id="projects" className="section-container" data-layout="column" data-layout-align="start center">
+          <div className="section projects" data-flex data-layout="column">
             <div className="section-header" data-layout="column" data-layout-align="center center">
-              <span className="title">Skills</span>
+              <span className="title">Projects</span>
               <span className="description">I have played with many technologies and love to learn more</span>
             </div>
-            <div class="skills-details" data-flex data-layout="column">
+            <div class="project-details" data-flex data-layout="column">
 
             </div>
           </div>
